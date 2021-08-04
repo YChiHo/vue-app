fa65a2dbe28840d5dcaee8ebcf92ae1c514f53f2
ych <chiho2736@gmail.com>
2021-08-04 10:44:47 +0900

    chatchoose 추가

---
+++ src/components/Common/SVG-image.vue
@@ -0,0 +1,43 @@
+<template>
+    <img :src="path" :alt="alt" :width="width" :height="height"/>
+</template>
+
+<script>
+export default {
+    name: 'SVG-image',
+    props: {
+        SVGFile: {
+            type: String,
+            default: '@/assets/logo.png'
+        },
+        alt: {
+            type: String,
+            defalt: 'image'
+        },
+        width: {
+            type: [Number, String],
+            default: 25
+        },
+        height: {
+            type: [Number, String],
+            default: 25
+        },
+    },
+    computed: {
+        path() {
+            return require(`@/assets/${this.SVGFile}`)
+        },
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+img {
+    max-width: 100%;
+    height: auto;
+    cursor: pointer;
+    margin: 10px;
+}
+
+</style>
---
+++ src/components/Common/icon-base.vue
@@ -0,0 +1,61 @@
+<template>
+    <svg xmlns="http://www.w3.org/2000/svg"
+    :width="width"
+    :height="height"
+    :viewBox="viewbox"
+    :aria-labelledby="iconName"
+    role="presentation"
+    >
+    <title
+        :id="iconName"
+        lang="en"
+    >{{ iconName }}</title>
+    <g :fill="iconColor">
+        <slot />
+    </g>
+    </svg>
+</template>
+
+<script>
+
+export default {
+    name: 'icon-base',
+    components: {
+    },
+    props: {
+        iconName: {
+            type: String,
+            default: 'icon'
+        },
+        width: {
+            type: [Number, String],
+            default: 18
+        },
+        height: {
+            type: [Number, String],
+            default: 18
+        },
+        iconColor: {
+            type: String,
+            default: 'currentColor'
+        },
+    },
+    computed: {
+        viewBox() {
+            return '0 0 ' + this.width + ' ' + this.height 
+        }
+    },
+    methods: {
+
+    },
+}
+</script>
+
+<style scoped>
+svg {
+    display: inline-block;
+    vertical-align: baseline;
+    margin-bottom: -2px; /* yes, I'm that particular about formatting */
+    cursor: pointer;
+}
+</style>
